ee0695d9ca38e8df3e7879493b34f54922cf2a44
Confused-Engineer <101591048+Confused-Engineer@users.noreply.github.com>
2024-05-31 21:08:36 -0400

    Add files via upload

---
+++ Encode Scripts/Encode Script.ps1
@@ -0,0 +1,3 @@
+﻿$PS1Script = Read-Host -Prompt "Powershell Script Path"
+$Base64 = [System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes([System.IO.File]::ReadAllText($PS1Script.Replace('"',""))))
+$Base64 | Out-File -FilePath "bytes.txt"
